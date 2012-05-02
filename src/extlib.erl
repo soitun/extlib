1938fd8372e8781f6639e97995ba7aa5f146b471
Ery Lee <ery.lee@gmail.com>
2012-05-02 10:12:07 +0800

    add module_with_attrs method

---
+++ src/extlib.erl
@@ -0,0 +1,29 @@
+-module(extlib).
+
+-compile(export_all).
+
+%copy from rabbitmq
+module_with_attrs(App, Name) ->
+	case application:get_key(App, modules) of
+	{ok, Modules} ->
+		lists:foldl(
+		  fun (Module, Acc) ->
+			  case lists:append([Atts || {N, Atts} <- module_attributes(Module),
+										 N =:= Name]) of
+			  []   -> Acc;
+			  Atts -> [{Module, Atts} | Acc]
+			  end
+		  end, [], Modules);
+	undefined ->
+		[]
+	end.
+
+%copy from rabbitmq
+module_attributes(Module) ->
+    case catch Module:module_info(attributes) of
+	{'EXIT', Reason} ->
+		exit(Reason);
+	V ->
+		V
+    end.
+
