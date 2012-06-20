3e1c9652a3a4c8bf2ba7c51015b02d1cb39231f2
Ery Lee <ery.lee@gmail.com>
2012-06-20 12:00:00 +0800

    add slice api

--- src/extlib.erl
+++ src/extlib.erl
@@ -2,6 +2,19 @@
 
 -compile(export_all).
 
+slice(Len, List) ->
+	slice(Len, List, []).
+
+slice(_Len, [], Acc) ->
+	lists:reverse(Acc);
+
+slice(Len, List, Acc) when length(List) =< Len ->
+	lists:reverse([List|Acc]);
+
+slice(Len, List, Acc) when length(List) > Len ->
+	{L1, L2} = lists:split(Len, List),
+	slice(Len, L2, [L1|Acc]).
+
 appvsn() ->
     {ok, App} = application:get_application(),
     case application:get_key(App, vsn) of
